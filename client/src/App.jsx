358f7885a87c2f9b106ae8ab2ae4c62347b26e19
NKRTECH <141981269+NKRTECH@users.noreply.github.com>
2024-01-30 21:06:44 +0530

    Destination.jsx added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,6 @@
 import './App.css';
+import DestinationCard from './Destination';
 
-const DestinationCard = ({ title, description, imageUrl }) => (
-  <div className="destination-card">
-    <img src={imageUrl} alt={`${title} Image`} className="destination-image" />
-    <h2 className="destination-title">{title}</h2>
-    <p className="destination-description">{description}</p>
-  </div>
-);
 
 const App = () => {
   return (
